3cd7e5f9acf377ea055f6ccc403da625c9a387cd
Max <maxtop4k022@gmail>
2025-07-31 11:05:22 +0300

    Initial Commit

---
+++ TODOListMVVMTests/TODOListMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListMVVMTests.swift
+//  TODOListMVVMTests
+//
+//  Created by admin on 31.07.2025.
+//
+
+import Testing
+@testable import TODOListMVVM
+
+struct TODOListMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
